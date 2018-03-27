2c89274d6ca11e8fd2601811052a927265079cd4
Omayeli Arenyeka <aomayeli@gmail.com>
2018-03-27 15:56:52 -0400

    Using ofMap to get mouse position in grid

--- knowlton-3/src/main.cpp
+++ knowlton-3/src/main.cpp
@@ -7,8 +7,8 @@
 int main( ){
     ofGLFWWindowSettings settings;
     
-    settings.width = 600;
-    settings.height = 600;
+    settings.width = 800;
+    settings.height = 800;
     settings.setPosition(ofVec2f(300,0));
     settings.resizable = true;
     shared_ptr<ofAppBaseWindow> mainWindow = ofCreateWindow(settings);
--- knowlton-3/src/ofApp.cpp
+++ knowlton-3/src/ofApp.cpp
@@ -8,7 +8,7 @@ void ofApp::setup(){
     
     cout << img.getWidth() << " " << img.getHeight() << endl;
     fbo.allocate(img.getWidth()*4, img.getHeight()*4, GL_RGBA, 4);
-    gridSize = 2;
+    gridSize = 5;
     
     // load all images
     for (int i = 0; i < 50; i++) {
@@ -52,23 +52,22 @@ void ofApp::draw(){
     int width = ofGetWidth();
     int height = img.getHeight() / img.getWidth() * width;
     fbo.draw(0,0, width, height);
-    int size = imgArr.size();
-    
-    // to account for the grid size (every 1px doesn't correspond to an image)
-    int revisedX = (int)mouseX/gridSize;
-    int revisedY = (int)mouseY/gridSize;
-    cout << size << endl;
-    cout << revisedY << " " << revisedX << endl;
+    int width2 = imgArr.size();
+    int height2 = imgArr[0].size();
+    int newMouseX = ofMap(mouseX, 0, width, 0, width2);
+    int newMouseY = ofMap(mouseY, 0, height, 0, height2);
+
+    cout << newMouseX << " " << newMouseY << endl;
 
     // make sure mouse is within the bounds of the image and also that the index is imgArr
-    if (mouseX < width && mouseY < height && revisedX < size && revisedY < size) {
+    if (mouseX >= 0 && mouseX < width && mouseY >= 0 && mouseY < height) {
         // pass the index of the image to the gui to draw
-        int idx = imgArr[mouseX][mouseY];
+        int idx = imgArr[newMouseX][newMouseY];
         if (idx <= 50) {
+            cout << idx << endl;
             gui->idx = idx;
         }
     }
-//
 }
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
